1f7588daf11110cea4ae8907b406a07e79d965bb
JuancaJcA <avilafloresjc@gmail.com>
2023-04-21 22:09:17 -0400

    Project created

---
+++ src/main/kotlin/com/example/swagdiscovery/SwagDiscoveryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.swagdiscovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwagDiscoveryApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwagDiscoveryApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/swagdiscovery/SwagDiscoveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.swagdiscovery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwagDiscoveryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
