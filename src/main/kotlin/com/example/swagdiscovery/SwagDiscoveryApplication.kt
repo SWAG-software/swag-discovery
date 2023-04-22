f6d00c9c95f779616e19d357e69b44c145071370
JuancaJcA <avilafloresjc@gmail.com>
2023-04-21 23:35:13 -0400

    Dockerfile created

--- src/main/kotlin/com/example/swagdiscovery/SwagDiscoveryApplication.kt
+++ src/main/kotlin/com/example/swagdiscovery/SwagDiscoveryApplication.kt
@@ -2,8 +2,10 @@ package com.example.swagdiscovery
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class SwagDiscoveryApplication
 
 fun main(args: Array<String>) {
